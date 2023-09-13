e745b07596e3be4c718d12acc18f53a2efd3d5fa
Abhishek Gupta <agabhishek20121998@gmail.com>
2023-09-13 07:13:25 -0400

    Added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { GeneralModule } from './general/general.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     GeneralModule,
     HomeModule,
     NgbModule,
