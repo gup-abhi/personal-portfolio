02e21d77fa71064d3c71a3d7ad597309658ba724
Abhishek Gupta <agabhishek20121998@gmail.com>
2023-09-13 06:01:36 -0400

    Added animations

--- src/app/home/experience/experience.component.ts
+++ src/app/home/experience/experience.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-experience',
@@ -10,6 +11,7 @@ export class ExperienceComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    AOS.init();
   }
 
   selectedTab(value: String) {
