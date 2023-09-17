7bbcb01cbabe3d71cff9f1d0cd9d0204d893c449
Abhishek Gupta <agabhishek20121998@gmail.com>
2023-09-16 21:20:31 -0400

    Hide images on mobile in project

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { EffectDirectiveDirective } from './directive/effect-directive.directive';
 import { TextLoopDirective } from './directive/text-loop.directive';
 import { HeadingComponent } from './components/heading/heading.component';
-import { HeadingService } from './services/heading.service';
 
 @NgModule({
   declarations: [EffectDirectiveDirective, TextLoopDirective, HeadingComponent],
