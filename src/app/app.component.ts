d85df0b66297a67ed7fa58018be80bb577100078
gup-abhi <agabhishek20121998@gmail.com>
2024-02-26 19:52:47 -0500

    added particle animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,8 @@ import { Title } from '@angular/platform-browser';
 import { DOCUMENT } from '@angular/common';
 import { AnalyticsService } from './shared/services/analytics.service';
 
+declare var particlesJS: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -18,6 +20,86 @@ import { AnalyticsService } from './shared/services/analytics.service';
 })
 export class AppComponent implements OnInit {
   title = 'Abhishek Gupta';
+  // ParticlesConfig for a Galaxy Ring
+  ParticlesConfig = {
+    particles: {
+      number: {
+        value: 100, // Number of particles
+        density: {
+          enable: false,
+        },
+      },
+      color: {
+        value: ['#ffffff'], // Colors for particles
+      },
+      shape: {
+        type: 'star', // Shape type set to 'star'
+        stroke: {
+          width: 0,
+          color: '#000000',
+        },
+      },
+      opacity: {
+        value: 1.1,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 1,
+          opacity_min: 0.2,
+          sync: true,
+        },
+      },
+      size: {
+        value: 3.5,
+        random: true,
+        anim: {
+          enable: true,
+          speed: 5,
+          size_min: 1,
+          sync: false,
+        },
+      },
+      line_linked: {
+        enable: false,
+      },
+      move: {
+        enable: true,
+        speed: 0.5, // Adjust the speed of particles
+        direction: 'bottom', // Make stars fall down
+        random: true,
+        straight: false,
+        out_mode: 'out',
+        attract: {
+          enable: false,
+        },
+      },
+    },
+    interactivity: {
+      detect_on: 'window',
+      events: {
+        onhover: {
+          enable: true,
+          mode: 'grab',
+        },
+        onclick: {
+          enable: false,
+        },
+        resize: true,
+      },
+    },
+    retina_detect: true,
+    background: {
+      color: '#000000', // Background color of the canvas
+    },
+    detectRetina: true,
+    fpsLimit: 60, // Adjust the frames per second limit
+    motion: {
+      reduce: {
+        factor: 2, // Adjust the motion reduce factor
+        value: true,
+      },
+    },
+  };
 
   constructor(
     private headingService: HeadingService,
@@ -42,6 +124,10 @@ export class AppComponent implements OnInit {
       duration: 1000,
       easing: 'ease-in-sine',
       delay: 100,
+    });
+
+    particlesJS('particles-js', this.ParticlesConfig, function () {
+      console.log('callback - particles.js config loaded');
     });
   }
 
